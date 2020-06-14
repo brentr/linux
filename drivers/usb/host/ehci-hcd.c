ffbf476f8c5f9affca3511fe580e2cdd12467dc4
Brent Roman <brent@mbari.org>
2020-06-13 17:28:52 -0700

    Missing CPU_LEDS, cs89x0 ethernet, serial DMA, ...

--- drivers/usb/host/ehci-hcd.c
+++ drivers/usb/host/ehci-hcd.c
@@ -1298,6 +1298,11 @@ MODULE_LICENSE ("GPL");
 #define	PLATFORM_DRIVER		ehci_hcd_sead3_driver
 #endif
 
+#ifdef CONFIG_USB_EHCI_LPC
+#include "ehci-lpc.c"
+#define	PLATFORM_DRIVER	ehci_lpc_driver
+#endif
+
 static int __init ehci_hcd_init(void)
 {
 	int retval = 0;
